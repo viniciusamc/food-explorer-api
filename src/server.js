0bef5e03da71cdd03c2ecfedb241babe2f784c33
vinicius <viniciusmartinsdacunha1@gmail.com>
2022-12-04 17:08:34 -0300

    test:routes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
-const { response } = require("express");
 const express = require("express");
+const routes = require("./routes/index.js")
 
 const app = express();
+app.use(express.json())
+
+app.use(routes)
 
 const port = 3333;
 app.listen(port, () => console.log(`Server is running on ${port}`))
