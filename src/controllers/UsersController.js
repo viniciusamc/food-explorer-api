6d007bcf17ea5e0f5291fb6ecf4f56500034e086
Vinicius <viniciusmartinsdacunha1@gmail.com>
2023-01-15 19:43:19 -0300

    feat:backend user verification

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,12 +1,31 @@
 const AppError = require("../utils/AppError");
+require database = require("../database/sqlite")
+
 
 class UserController{
-  create(req, res){
+  async create(req, res){
     const { name, email, password } = req.body;
+    const emailVerify = new RegExp (/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
 
     if(!name || !email || !password){
       throw new AppError("Preencha todas as caixas corretamente");
     }
+
+    if(name.length < 3) {
+      throw new AppError("Desculpe, o nome precisa ter pelo menos 3 caracteres para ser válido. Por favor, tente novamente. ");
+    }
+
+    if(password.length < 6) {
+      throw new AppError("A senha deve ter no mínimo 6 caracteres.");
+    }
+
+    if(!emailVerify.test(email)) {
+      throw new AppError("Endereço de e-mail inválido");
+    }
+    
+    await database.run("INSERT INTO users (name, email, password) VALUES (?,?,?)",
+    [name, email, password]
+    )
     
     res.status(201).json({name, email, password}) 
   }
