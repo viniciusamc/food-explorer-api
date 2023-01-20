2cb0d17d33f8cbe3ebf08ed9f176fafde4e272ba
Vinicius <viniciusmartinsdacunha1@gmail.com>
2023-01-20 02:11:34 -0300

    feat:sessions and middleware

---
+++ src/middleware/ensureAuthenticated.js
@@ -0,0 +1,29 @@
+const { verify } = require("jsonwebtoken");
+const AppError = require("../utils/AppError");
+const authConfig = require("../configs/auth")
+
+function ensureAuthenticated(req, res, next)
+{
+  const authHeader = req.headers.authorization;
+
+  if(!authHeader)
+  {
+    throw new AppError("Token Inválido")
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try{
+    const {sub : user_id} = verify(token, authConfig.jwt.secret);
+
+    request.user = {
+      id: Number(user_id),
+    };
+
+    return next();
+  }catch{
+    throw new AppError("JWT Token Invalido", 401)
+  }
+}
+
+module.exports = ensureAuthenticated;
