72a7bcf80ce8ce5d18b980e19dc8604af86977dd
Vinicius <viniciusmartinsdacunha1@gmail.com>
2023-02-10 17:12:47 -0300

    fix:ensure

--- src/middleware/ensureAuthenticated.js
+++ src/middleware/ensureAuthenticated.js
@@ -1,28 +1,26 @@
 const { verify } = require("jsonwebtoken");
 const AppError = require("../utils/AppError");
-const authConfig = require("../configs/auth")
+const authConfig = require("../configs/auth");
 
-function ensureAuthenticated(req, res, next)
-{
+function ensureAuthenticated(req, res, next) {
   const authHeader = req.headers.authorization;
 
-  if(!authHeader)
-  {
-    throw new AppError("Token Inválido")
+  if (!authHeader) {
+    throw new AppError("Token Inválido");
   }
 
   const [, token] = authHeader.split(" ");
 
-  try{
-    const {sub : user_id} = verify(token, authConfig.jwt.secret);
+  try {
+    const { sub: user_id } = verify(token, authConfig.jwt.secret);
 
     request.user = {
       id: Number(user_id),
     };
 
     return next();
-  }catch{
-    throw new AppError("JWT Token Invalido", 401)
+  } catch {
+    throw new AppError("JWT Token Invalido", 401);
   }
 }
 
