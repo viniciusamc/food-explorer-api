1974cfc5d88de0672db37616d3e18e1ee099b8c3
Vinicius <viniciusmartinsdacunha1@gmail.com>
2023-03-07 11:37:58 -0300

    create cart

--- src/controllers/CartController.js
+++ src/controllers/CartController.js
@@ -1,6 +1,15 @@
+const AppError = require("../utils/AppError");
+const knex = require("../database/knex");
+
 class CartController {
   async create(req, res) {
-    const { id } = req.body;
+    const { id, meal_id } = req.body;
+
+    if (!id || !meal_id) {
+      throw new AppError("Id Not Found");
+    }
+
+    await knex("cart").insert({ user_id: id, meal_id });
 
     return res.status(201).json({ id });
   }
