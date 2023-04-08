1a0abfac2a6d7ad1197c8afb6aad34f65f6b9b09
Vinicius <viniciusmartinsdacunha1@gmail.com>
2023-04-07 23:56:18 -0300

    feat:cors

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
-require('express-async-errors')
+require("express-async-errors");
 // const database = require("./database/sqlite")
 const express = require("express");
+const cors = require("cors");
 
 const routes = require("./routes/index.js");
 
 const AppError = require("./utils/AppError.js");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
@@ -16,17 +18,17 @@ app.use(routes);
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({
-      status: 'error',
-      statusCode: error.message
-    })
+      status: "error",
+      statusCode: error.message,
+    });
   }
 
-  console.error(error)
+  console.error(error);
 
   return response.status(500).json({
-    message: 'internal server error'
-  })
-})
+    message: "internal server error",
+  });
+});
 
 const port = 3333;
-app.listen(port, () => console.log(`Server is running on ${port}`))
+app.listen(port, () => console.log(`Server is running on ${port}`));
