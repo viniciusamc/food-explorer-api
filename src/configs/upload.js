928a6b8523fa7e95f21019d67cd6816ae4b139ce
Vinicius <viniciusmartinsdacunha1@gmail.com>
2023-04-09 00:07:39 -0300

    feat:images

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -1,5 +1,24 @@
-// const path = require("path");
+const path = require("path");
+const multer = require("multer");
+const crypt = require("crypto");
 
-// const TPM_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
+const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
+const UPLOADS_FOLDER = path.resolve(__dirname, "uploads");
 
-// const UPLOADS_FOLDER = path.resolve(__dirname, "");
+const MULTER = {
+  storage: multer.diskStorage({
+    destination: TMP_FOLDER,
+    filename: (req, file, cb) => {
+      const fileHash = crypt.randomBytes(2).toString("hex");
+      const fileName = `${fileHash}-${file.originalname}`;
+
+      return cb(null, fileName);
+    },
+  }),
+};
+
+module.exports = {
+  TMP_FOLDER,
+  UPLOADS_FOLDER,
+  MULTER,
+};
